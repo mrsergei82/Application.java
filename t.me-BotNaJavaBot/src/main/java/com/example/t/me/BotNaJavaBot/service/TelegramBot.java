701fe5550fdf4468d2e653aa234900685c0911fc
root <root@DESKTOP-PUATC74.localdomain>
2022-12-18 21:24:49 +0300

    Initial commit

---
+++ t.me-BotNaJavaBot/src/main/java/com/example/t/me/BotNaJavaBot/service/TelegramBot.java
@@ -0,0 +1,121 @@
+package com.example.t.me.BotNaJavaBot.service;
+
+
+import com.example.t.me.BotNaJavaBot.config.BotConfig;
+import com.example.t.me.BotNaJavaBot.model.User;
+import com.example.t.me.BotNaJavaBot.model.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    final BotConfig config;
+    static final String HELP_TEXT = "This bot is created to demonstrate Spring capabilities.\n \n"+
+            "You can execute commands from the main menu on the left or by typing a command: \n\n"+
+            "Type /start to see a welcome message\n\n"+
+            "Type /mydata to see data stored about yourself\n\n"+
+            "Type /help to see this message again";
+
+    public TelegramBot(BotConfig config){
+
+        this.config = config;
+        List<BotCommand> listofCommands = new ArrayList<>();
+        listofCommands.add(new BotCommand("/start","get a welcome message"));
+        listofCommands.add(new BotCommand("/mydata","gey your data"));
+        listofCommands.add(new BotCommand("/deletedata","delete my data"));
+        listofCommands.add(new BotCommand("/help","info how to use this bot"));
+        listofCommands.add(new BotCommand("/settings","set your preferences"));
+        try {
+            this.execute(new SetMyCommands(listofCommands, new BotCommandScopeDefault(),null));
+        }
+        catch (TelegramApiException e) {
+            log.error("Error setting bot's command list: "+ e.getMessage());
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            switch (messageText){
+                case "/start":
+
+                    registerUser(update.getMessage());
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                case "/help":
+                    sendMessage(chatId, HELP_TEXT);
+                    break;
+                default: sendMessage(chatId,"Sorry");
+            }
+        }
+
+    }
+    private void registerUser(Message msg){
+        if (userRepository.findById(msg.getChatId()).isEmpty()){
+            var chatId = msg.getChatId();
+            var chat = msg.getChat();
+
+            User user = new User();
+            user.setChatId(chatId);
+            user.setFirstName(chat.getFirstName());
+            user.setLastName(chat.getLastName());
+            user.setUserName(chat.getUserName());
+            user.setRegisteredAt(new Timestamp(System.currentTimeMillis()));
+
+            userRepository.save(user);
+            log.info("user saved: "+user);
+        }
+    }
+    private void startCommandReceived(long chatId, String name){
+
+        String answer = "Hi "+name+" How are you?";
+
+        log.info("Replied to user " + name);
+
+        sendMessage(chatId, answer);
+    }
+    private void sendMessage(long chatId, String textToSend){
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+
+        try {
+            execute(message);
+        }
+        catch (TelegramApiException e){
+            log.error("Error occurred: " + e.getMessage());
+        }
+    }
+}
---
+++ t.me-BotNaJavaBot/src/test/java/com/example/t/me/BotNaJavaBot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.t.me.BotNaJavaBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
